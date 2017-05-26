38935889f7d201f80693a60878686c26ccb920bb
Jonathan <jiquinta@espol.edu.ec>
2017-05-25 20:01:58 -0500

    modificacion de lectura de archivos y creacion de modulo usuarios

---
+++ login.py
@@ -0,0 +1,4 @@
+import usuarios
+
+def InicioDeSesion (usuario,contrasena):
+
--- usuarios.py
+++ usuarios.py
@@ -1,23 +1,4 @@
-archivoUsuarios = open("usuarios.txt","r")
+import funcionesDeLectura
 usuarios = dict()
 usuarios = {}
-claves =[]
-linea2 = []
-linea3 = []
-key1 = []
-linea = archivoUsuarios.readline()
-while linea != "":
-    #creacion de una lista con valores de la linea
-    claves = linea.split(",")
-    #creacion de lista con los valores de CI o Id para usar como key
-    key1.append(claves[0])
-    #asignacion de valores en una lista de lista
-    for i in range(len(claves)):
-        if (claves[i] != "\n") and (claves[i] != ""):
-            linea2.append(claves[i])
-    linea3.append(linea2)
-    linea2 = []
-    linea = archivoUsuarios.readline()
-#asignacion de valores al diccionario
-for i in range(len(linea3)):
-    usuarios.setdefault(key1[i],linea3[i])
+funcionesDeLectura.leerArchivo("usuarios.txt",usuarios)
