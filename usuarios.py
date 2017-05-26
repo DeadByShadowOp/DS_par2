da6213cae81ce2786732f8853c2b589319657e50
Jonathan <jiquinta@espol.edu.ec>
2017-05-25 19:32:46 -0500

    add de lectura de usuarios

---
+++ usuarios.py
@@ -0,0 +1,23 @@
+archivoUsuarios = open("usuarios.txt","r")
+usuarios = dict()
+usuarios = {}
+claves =[]
+linea2 = []
+linea3 = []
+key1 = []
+linea = archivoUsuarios.readline()
+while linea != "":
+    #creacion de una lista con valores de la linea
+    claves = linea.split(",")
+    #creacion de lista con los valores de CI o Id para usar como key
+    key1.append(claves[0])
+    #asignacion de valores en una lista de lista
+    for i in range(len(claves)):
+        if (claves[i] != "\n") and (claves[i] != ""):
+            linea2.append(claves[i])
+    linea3.append(linea2)
+    linea2 = []
+    linea = archivoUsuarios.readline()
+#asignacion de valores al diccionario
+for i in range(len(linea3)):
+    usuarios.setdefault(key1[i],linea3[i])
