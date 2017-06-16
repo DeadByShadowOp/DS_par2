9a43933cbe8c32f13186c9675942748af56d7f7c
bernix01 <gbernal096@gmail.com>
2017-06-15 19:54:01 -0500

    some views added

--- v2/views.py
+++ v2/views.py
@@ -1,6 +1,8 @@
 from utils import showPrettyTitle, freeTextInput, choiceInput
 import Auth
 import FeikDatabase
+from controllers import buscarPlatilloPorNombre, buscarPlatilloPorDescripcion
+
 
 def login_view():
     showPrettyTitle("INICIAR SESION")
@@ -11,23 +13,49 @@ def login_view():
         user = Auth.authenticate(usuario, psw)
         if user:
             print("Bienvenido " + user.nombre)
-            return True
+            break
+        print("Datos ingresados erroneos")
     return user
 
+
 @Auth.require_login
-def saludar():
+def admin_view():
     print("hola")
+
+
+def categorias_view():
+    showPrettyTitle("Categorías de platillos")
+    for categorias in FeikDatabase.FeikDatabase.data["categorias"]:
+        print(categorias)
+    input("asd")
+
+
+def buscarPorNombre_view():
+    nombre = freeTextInput("Ingrese el nombre")
+    print(buscarPlatilloPorNombre(nombre))
+
+
+def buscarPorDescripcion_view():
+    descripcion = freeTextInput("Ingrese la descripcion")
+    print(buscarPlatilloPorDescripcion(descripcion))
+
+
+def buscar_view():
+    showPrettyTitle("Buscar Platillo")
+    menu = "01.- Por nombre\n02.- Por descripción\n03.- Volver"
 
 
 def main_view():
     showPrettyTitle("Restaurantes ESPOL")
-    menu = "01. Saludar\n"
+    menu = "01.- Listar Categorías De Platos\n02.- Buscar Plato\n03.- Administracion"
     acciones = {
-        0: saludar
+        0: categorias_view,
+        1: buscar_view,
+        3: admin_view
     }
     opt = choiceInput(menu, 1)
     if opt:
-        acciones[int(opt)-1]()
+        acciones[int(opt) - 1]()
     else:
         return False
     return True
